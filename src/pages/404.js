c45e07b6f3d4309220a50c0ae7c0920f010c8395
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-08-12 14:42:15 -0300

    Trying fix bug in page 404 not work

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,34 +1,5 @@
-import Link from 'next/link'
-import {createGlobalStyle} from 'styled-components'
-import ContainerParticles from '../components/ContainerParticles/'
+import Error from 'next/error';
 
-import GlobalStyle from '../styles/GlobalStyle'
-import {
-	Container,
-	TextError,
-	BackToHome,
-} from '../styles/pages/404'
-
-const CustomStyles = createGlobalStyle`
-	body, html {
-		overflow: hidden;
-	}
-`
-
-export default function Error404() {
-	return (
-		<>
-			<Container>
-				<TextError data-text="Error 404">Error 404</TextError>
-				<h1>Página não encontrada</h1>
-				<Link href="/"><a style={{
-					margin: '25px 0',
-				}}><BackToHome>Go to home</BackToHome></a></Link>
-			</Container>
-			<ContainerParticles />
-
-			<CustomStyles />
-			<GlobalStyle />
-		</>
-	)
+export default function NotFound() {
+  return <Error statusCode={404} title="This tweet could not be found"></Error>;
 }
