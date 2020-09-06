9b852d1ea0169c23d3cfa6a319b12a699eb2fe3f
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-09-06 13:08:22 -0300

    Add reCaptcha feature

--- pages/signin/index.js
+++ pages/signin/index.js
@@ -5,6 +5,7 @@ import Head from 'next/head'
 import {ThemeContext} from 'styled-components'
 import { RiMapPinUserLine, RiLockPasswordLine } from 'react-icons/ri'
 import { FaHome } from 'react-icons/fa'
+import ReCAPTCHA from "react-google-recaptcha";
 
 import {
 	Container,
@@ -23,7 +24,7 @@ import {
 import GlobalStyle from '../../styles/GlobalStyle'
 
 const About = () => {
-	const {colors} = useContext(ThemeContext);
+	const theme = useContext(ThemeContext);
 
 	return (
 		<div className="container">
@@ -36,16 +37,16 @@ const About = () => {
 				<meta name="description" content="Acesse sua conta, aqui você pode obter todas as informações disponíveis para o uso dos seus produtos adquiridos." />
 				<meta name="Description" content="Acesse sua conta, aqui você pode obter todas as informações disponíveis para o uso dos seus produtos adquiridos." />
 				<meta name="twitter:description" content="Acesse sua conta, aqui você pode obter todas as informações disponíveis para o uso dos seus produtos adquiridos." />
-				<meta name="theme-color" content={colors.background} />
-				<meta name="apple-mobile-web-app-status-bar-style" content={colors.background} />
-				<meta name="msapplication-navbutton-color" content={colors.background} />
+				<meta name="theme-color" content={theme.colors.background} />
+				<meta name="apple-mobile-web-app-status-bar-style" content={theme.colors.background} />
+				<meta name="msapplication-navbutton-color" content={theme.colors.background} />
 			</Head>
 
 			<Container>
 				<Link href="/">
 					<a>
 						<ContainerHeaderLogo>
-							<FaHome size="52px" color={colors.secundaryBackground} />
+							<FaHome size="52px" color={theme.colors.secundaryBackground} />
 						</ContainerHeaderLogo>
 					</a>
 				</Link>
@@ -57,17 +58,26 @@ const About = () => {
 						<Form>
 							<LabelInput>Informe seu usuário</LabelInput>
 							<InputContainer>
-								<RiMapPinUserLine style={{marginLeft: "0.5rem", position: "absolute"}} color={colors.background} size="1.5em" />
+								<RiMapPinUserLine style={{marginLeft: "0.5rem", position: "absolute"}} color={theme.colors.background} size="1.5em" />
 								<InputForm placeholder="Digite o username de sua conta" />
 							</InputContainer>
 							<LabelInput>Informe sua senha</LabelInput>
 							<InputContainer>
-								<RiLockPasswordLine style={{marginLeft: "0.5rem", position: "absolute"}} color={colors.background} size="1.5em" />
+								<RiLockPasswordLine style={{marginLeft: "0.5rem", position: "absolute"}} color={theme.colors.background} size="1.5em" />
 								<InputForm placeholder="Digite a senha de sua conta" />
 							</InputContainer>
 							<Link href="/forgot">
 								<ForgotPassword href="/forgot">Esqueceu sua senha?</ForgotPassword>
 							</Link>
+							<ReCAPTCHA
+								style={{
+									display: 'flex',
+									justifyContent: 'center',
+									marginTop: '15px'
+								 }}
+								theme={theme.title.toLowerCase()}
+								sitekey="6LeSj8gZAAAAAMhcGwNQOwkGaYJd7wElVYW0Zbeb"
+							/>
 						</Form>
 					</ContainerBody>
 				</ContainerLogin>
