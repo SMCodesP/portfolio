1f4ddadaf57edd660feb677c415ffd5b643dda12
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-10-19 12:44:40 +0000

    Start theme of dark

--- utils/usePersistedState.js
+++ utils/usePersistedState.js
@@ -1,13 +1,15 @@
 import { useState, useEffect } from 'react';
 import Cookie from 'js-cookie';
 
-function usePersistedState(key, initialState) {
+function usePersistedState(key, initialState, inverse) {
 	const [state, setState] = useState(initialState)
 
   useEffect(() => {
 		if (localStorage.getItem(key)) {
       setState(JSON.parse(localStorage.getItem(key)))
-    }
+    } else {
+			setState(inverse)
+		}
   }, [])
 
 	return [state, setState]
