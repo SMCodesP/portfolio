dcfd678a1836642c9fa1a159bf6746502bf31ccd
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-11-03 09:34:39 -0300

    Component listing pages to css

--- components/ListingPages/index.js
+++ components/ListingPages/index.js
@@ -3,13 +3,10 @@ import {useContext} from 'react'
 import {ThemeContext} from 'styled-components'
 import {FiChevronRight, FiChevronLeft} from 'react-icons/fi'
 
-import {
-	ContainerNextPrevious,
-	TextPrevNext,
-	ContainerListPages,
-	ListProductsPages,
-	ProductPage,
-} from './style.js'
+import {shade} from 'polished'
+
+import getColor from '../../utils/getColor'
+import styles from './ListingPages.module.css'
 
 export default ({product, ...params}) => {
 	const {colors} = useContext(ThemeContext)
@@ -18,39 +15,53 @@ export default ({product, ...params}) => {
 	const listPages = listPagesQuantity.filter((value) => value < params.id+6 && value > params.id-6)
 	
 	return (
-		<ContainerNextPrevious>
+		<div className={styles.container_next_previous}>
 			{(Number(params.id)-1 >= 0) ? (
 				<Link href={`/${product.category}/[id]`} as={`/${product.category}/${params.id-1}`}>
-					<TextPrevNext href={`/${product.category}/${params.id-1}`}>
+					<a className={styles.text_prev_next} href={`/${product.category}/${params.id-1}`}>
 						<FiChevronLeft color={colors.primary} size={24} />
 						<span>Anterior</span>
-					</TextPrevNext>
+					</a>
 				</Link>
 			) : (<span />)}
-			<ContainerListPages>
-				<ListProductsPages>
+			<div className={styles.container_list_pages}>
+				<ul className={styles.list_products_pages}>
 					{listPages.map((productId) => (productId != params.id) ? (
 						<Link href={`/${product.category}/[id]`} as={`/${product.category}/${productId}`} key={productId}>
 							<a href={`/${product.category}/${productId}`}>
-								<ProductPage>{productId}</ProductPage>
+								<li
+									className={styles.product_page}
+									style={{
+										boxShadow: `0 0 2px 1px ${shade(0.1, getColor('secundaryBackground'))}`,
+										background: shade(0.1, getColor('background'))
+									}}
+								>
+									{productId}
+								</li>
 							</a>
 						</Link>
 					) : (
-						<ProductPage key={productId} style={{
-							cursor: 'not-allowed',
-							filter: 'brightness(80%)'
-						}}>{productId}</ProductPage>
+						<li
+							className={styles.product_page}
+							key={productId}
+							style={{
+								boxShadow: `0 0 2px 1px ${shade(0.1, getColor('secundaryBackground'))}`,
+								background: shade(0.1, getColor('background')),
+								cursor: 'not-allowed',
+								filter: 'brightness(80%)'
+							}}
+						>{productId}</li>
 					))}
-				</ListProductsPages>
-			</ContainerListPages>
+				</ul>
+			</div>
 			{(params.quantity > Number(params.id)+1) ? (
 				<Link href={`/${product.category}/[id]`} as={`/${product.category}/${Number(params.id)+1}`}>
-					<TextPrevNext href={`/${product.category}/${Number(params.id)+1}`}>
+					<a className={styles.text_prev_next} href={`/${product.category}/${Number(params.id)+1}`}>
 						<span>Próximo</span>
 						<FiChevronRight color={colors.primary} size={24} />
-					</TextPrevNext>
+					</a>
 				</Link>
 			) : (<span />)}
-		</ContainerNextPrevious>
+		</div>
 	)
 }
