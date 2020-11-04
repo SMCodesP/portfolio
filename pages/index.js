6dab3efe1c148e16b04e054036efacabebe724a0
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-11-03 22:07:56 -0300

    Return footer in index

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
+import { useContext } from 'react'
 import Head from 'next/head'
+
+import {ThemeContext} from 'styled-components'
 
 import Navigation from '../components/Navigation/'
 import Products from '../components/Products'
@@ -8,7 +11,15 @@ import GlobalStyle from '../styles/GlobalStyle'
 
 import products from '../utils/products'
 
+import {
+	ContainerLicense,
+	Description,
+	Title,
+} from '../styles/pages/index'
+
 const Home = () => {
+	const theme = useContext(ThemeContext)
+
 	return (
 			<div className="container">
 				<Head>
@@ -20,9 +31,9 @@ const Home = () => {
 					<meta name="description" content="Portfólio de SMCodes, um programador web e mobile, venha comprar e baixar plugins grátis de Minecraft ou até mesmo encomendar softwares únicos." />
 					<meta name="Description" content="Portfólio de SMCodes, um programador web e mobile, venha comprar e baixar plugins grátis de Minecraft ou até mesmo encomendar softwares únicos." />
 					<meta name="twitter:description" content="Portfólio de SMCodes, um programador web e mobile, venha comprar e baixar plugins grátis de Minecraft ou até mesmo encomendar softwares únicos." />
-					{/* <meta name="theme-color" content={colors.background} /> */}
-					{/* <meta name="apple-mobile-web-app-status-bar-style" content={colors.background} /> */}
-					{/* <meta name="msapplication-navbutton-color" content={colors.background} /> */}
+					<meta name="theme-color" content={theme.colors.background} />
+					<meta name="apple-mobile-web-app-status-bar-style" content={theme.colors.background} />
+					<meta name="msapplication-navbutton-color" content={theme.colors.background} />
 				</Head>
 
 				<Navigation page="/" />
@@ -38,13 +49,13 @@ const Home = () => {
 						limit={true}
 					/>
 				))}
-				{/* <ContainerLicense> */}
-				{/* 	<Title>Sistema de licença</Title> */}
-				{/* 	<Description> */}
-				{/* 		Nosso sistema de licença apresenta com uma api focada em performance, todos os plugins de minecraft contém uma versão gratuita, porém caso compre a versão paga você adquiri 3 versões do mesmo plugin dedicado de acordo com sua hospedagem, você também recebe um licença dedicada para otimização do seu plugin, essa licença só pode ser usada em 1 servidor por vez. */}
-				{/* 	</Description> */}
-				{/* </ContainerLicense> */}
-				{/* <Footer /> */}
+				<ContainerLicense>
+					<Title>Sistema de licença</Title>
+					<Description>
+						Nosso sistema de licença apresenta com uma api focada em performance, todos os plugins de minecraft contém uma versão gratuita, porém caso compre a versão paga você adquiri 3 versões do mesmo plugin dedicado de acordo com sua hospedagem, você também recebe um licença dedicada para otimização do seu plugin, essa licença só pode ser usada em 1 servidor por vez.
+					</Description>
+				</ContainerLicense>
+				<Footer />
 
 				<GlobalStyle />
 			</div>
