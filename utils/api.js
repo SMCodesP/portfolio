0b8caccaa40823a5855d7818015bc722d8feb86a
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-11-29 14:11:05 -0300

    Change api

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: process.env.API || "https://portfolio-backend-sm.herokuapp.com"
+	baseURL: process.env.API || "https://morning-headland-07678.herokuapp.com"
 })
 
 export default api;
