71207038282af033b8ec8d13ee134c381c8b0937
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-11-30 14:17:13 -0300

    Remove statics categories of products

--- pages/[link].js
+++ pages/[link].js
@@ -29,11 +29,11 @@ function Plugins({ categories, category }) {
 				<title>{`SMCodes - ${category.title}`}</title>
 				<meta property="og:title" content={`SMCodes - ${category.title}`} key="title" />
 				<meta name="twitter:title" content={`SMCodes - ${category.title}`} />
-				<meta name="description" content="Venha comprar meus plugins, contamos com alto desempenho, sistemas inovadores, suporte rápido e api fácil para novas integrações." />
-				<meta property="og:description" content="Venha comprar meus plugins, contamos com alto desempenho, sistemas inovadores, suporte rápido e api fácil para novas integrações." />
-				<meta name="description" content="Venha comprar meus plugins, contamos com alto desempenho, sistemas inovadores, suporte rápido e api fácil para novas integrações." />
-				<meta name="Description" content="Venha comprar meus plugins, contamos com alto desempenho, sistemas inovadores, suporte rápido e api fácil para novas integrações." />
-				<meta name="twitter:description" content="Venha comprar meus plugins, contamos com alto desempenho, sistemas inovadores, suporte rápido e api fácil para novas integrações." />
+				<meta name="description" content={category.description} />
+				<meta property="og:description" content={category.description} />
+				<meta name="description" content={category.description} />
+				<meta name="Description" content={category.description} />
+				<meta name="twitter:description" content={category.description} />
 				<meta name="theme-color" content={colors.background} />
 				<meta name="apple-mobile-web-app-status-bar-style" content={colors.background} />
 				<meta name="msapplication-navbutton-color" content={colors.background} />
@@ -44,20 +44,13 @@ function Plugins({ categories, category }) {
 			</div>
 
 			<Container>
-				<ProgressiveImage
-					src="/construct.webp"
-					placeholder="/construct_min.webp"
-				>
-					{(src, loading) => (
-						<ImageLogo
-							loading={loading}
-							src={src}
-							alt="Construction picture image logo"
-						/>
-					)}
-				</ProgressiveImage>
+					<ImageLogo
+						src={category.banner_url}
+						loading={false}
+						alt="Construction picture image logo"
+					/>
 				<Title>{category.title}</Title>
-				<SubTitle>Plugins otimizados para seu servidor.</SubTitle>
+				<SubTitle>{category.description}</SubTitle>
 			</Container>
 
 			<Products
