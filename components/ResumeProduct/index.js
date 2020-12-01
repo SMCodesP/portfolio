20581cef5ec85933aa299359f3e33adee7902433
SMCodesP <samuel.silva513@etec.sp.gov.br>
2020-12-01 09:50:38 -0300

    Remove loading in page all not using

--- components/ResumeProduct/index.js
+++ components/ResumeProduct/index.js
@@ -1,10 +1,6 @@
 import Link from 'next/link'
-import {useContext} from 'react'
 
 import ProgressiveImage from 'react-progressive-graceful-image'
-import {shade} from 'polished'
-import {ThemeContext} from 'styled-components'
-import Skeleton, {SkeletonTheme} from 'react-loading-skeleton'
 
 import RenderMarkdown from '../RenderMarkdown/'
 
@@ -14,62 +10,43 @@ import {
 	Title,
 } from './styles'
 
-function ResumeProduct({product, loading}) {
-	const {colors} = useContext(ThemeContext)
+function ResumeProduct({product}) {
 
 	return (
 		<Container>
-			{loading ? (
-				<SkeletonTheme
-					color={shade(0.1, colors.secundaryBackground)}
-					highlightColor={shade(0.1, colors.background)}
-				>
-					<Skeleton width={142} height={132} duration={2} style={{
-						padding: '10px',
-						borderRadius: '5px',
-					}} />
-					<br />
-					<Skeleton width={142} height={12} duration={2} style={{
-						marginTop: '5px',
-						padding: '10px',
-						borderRadius: '5px',
-					}} />
-				</SkeletonTheme>
-			) : (
-				<>
-					<Link href={`/products${product.category.link}/[id]`} as={`/products${product.category.link}/${product.id}`}>
-						<a href={`/products${product.category.link}/${product.id}`}>
-							<ProgressiveImage
-								src={product.image_large}
-								placeholder={product.image_small}
-							>
-								{(src, loading) => product.image_size ? (
-									<ImageLogo
-										style={{
-											width: 142,
-											filter: loading ? 'blur(5px)' : ''
-										}}
-										not_auto={true}
-										src={src}
-										alt={`${product.title} logo image`}
-									/>
-								) : (
-									<ImageLogo
-										style={{
-											width: 142,
-											filter: loading ? 'blur(10px)' : ''
-										}}
-										not_auto={false}
-										src={src}
-										alt={`${product.title} logo image`}
-									/>
-								)}
-							</ProgressiveImage>
-						</a>
-					</Link>
-					<RenderMarkdown text={'`'+product.title+'`'} />
-				</>
-			)}
+			<>
+				<Link href={`/products${product.category.link}/[id]`} as={`/products${product.category.link}/${product.id}`}>
+					<a href={`/products${product.category.link}/${product.id}`}>
+						<ProgressiveImage
+							src={product.image_large}
+							placeholder={product.image_small}
+						>
+							{(src, loading) => product.image_size ? (
+								<ImageLogo
+									style={{
+										width: 142,
+										filter: loading ? 'blur(5px)' : ''
+									}}
+									not_auto={true}
+									src={src}
+									alt={`${product.title} logo image`}
+								/>
+							) : (
+								<ImageLogo
+									style={{
+										width: 142,
+										filter: loading ? 'blur(10px)' : ''
+									}}
+									not_auto={false}
+									src={src}
+									alt={`${product.title} logo image`}
+								/>
+							)}
+						</ProgressiveImage>
+					</a>
+				</Link>
+				<RenderMarkdown text={'`'+product.title+'`'} />
+			</>
 		</Container>
 	)
 }
