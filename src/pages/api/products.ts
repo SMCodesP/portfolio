3566fef87c83c488816ef0821132e7adbada0973
SMCodesP <samuel.silva513@etec.sp.gov.br>
2023-06-08 13:14:07 -0300

    fix: products api route to using connect

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -1,7 +1,21 @@
-import nc from 'next-connect';
-import projects from '@/public/projects.json';
-import { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { createRouter } from 'next-connect';
 
-export default nc<NextApiRequest, NextApiResponse>().get((req, res) => {
+import projects from '@/public/projects.json';
+
+const router = createRouter<NextApiRequest, NextApiResponse>();
+
+router.get((req, res) => {
   return res.json(projects);
+});
+
+export const config = {
+  runtime: `edge`,
+};
+
+export default router.handler({
+  onError: (err, req, res) => {
+    console.error(err.stack);
+    res.status(err.statusCode || 500).end(err.message);
+  },
 });
